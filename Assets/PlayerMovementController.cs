6b2e73318c646c6a6cb0f942c8354f35e9cbbfa4
David Jamgochian <jammer888@verizon.net>
2018-04-14 15:08:33 -0700

    Added a player prefab with networked movement
    Added a player prefab with networked movement. NOTE: in order to use player in other scenes, make sure that scene has an empty gameobject that has the needed network components attached. (See networking tutorial for more details).

---
+++ Assets/PlayerMovementController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+// Based off of unity networking tutorial (so far)
+public class PlayerMovementController : NetworkBehaviour {
+
+	public float rotationSpeed = 150.0f;
+	public float movementSpeed = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!isLocalPlayer) {
+			return;
+		}
+
+		float yRotation = Input.GetAxis("Horizontal") * Time.deltaTime * rotationSpeed;
+		float zMovement = Input.GetAxis("Vertical") * Time.deltaTime * movementSpeed;
+
+		transform.Rotate(0, yRotation, 0);
+		transform.Translate(0, 0, zMovement);
+	}
+}
