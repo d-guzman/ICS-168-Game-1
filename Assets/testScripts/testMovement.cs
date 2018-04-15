a6c4e83cc6a85d4b925a4b9b43ac56342582efc3
Nikhil Sharma <nik41507@gmail.com>
2018-04-15 16:06:39 -0700

    Made a tiny little level
    There is a small test level in a new scene with a moving character.
    
    -It can be controlled by clicking the mouse or WASD and the map uses NavMeshes.
    
    -The mouse-click controls have working stairs and jumping from platform to platform (not working for WASD yet)
    
    -I named all my folders testScenes, test Scripts, testMaterials etc to avoid any naming conflicts
    
    -I downloaded some extra stuff (NavMeshComponents) from the Unity Github
    
    -Let me know if there are any issues/ if you have any suggestions

---
+++ Assets/testScripts/testMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+//Here are some bareones WASD controls 
+public class testMovement : MonoBehaviour {
+
+    public float speed;
+
+    public Camera cam;
+
+    public NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+
+
+                
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+        transform.Translate(speed * moveX * Time.deltaTime, 0f, speed * moveZ * Time.deltaTime);
+        if (moveX != 0 || moveZ != 0)
+        {
+            transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+            agent.SetDestination(transform.position);
+        }
+       
+            
+        
+
+        if (Input.GetMouseButton(0))
+        {
+            RaycastHit hit;
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {       //Now the agent is moved
+                agent.SetDestination(hit.point);
+
+
+            }
+        }
+    }
+}
