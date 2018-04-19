33a984afe01e21e2f2daecd9a215b79a56718312
David Jamgochian <jammer888@verizon.net>
2018-04-19 16:35:59 -0700

    Created new player prefab that has updated movement and looking.
    Created new player prefab that has updated movement and looking. This prefab does not have networking implemented yet.

---
+++ Assets/Scripts/David-Scripts/testCharacterDirection.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testCharacterDirection : MonoBehaviour {
+
+	public Camera cam;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit hit;
+		Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+		if (Physics.Raycast(ray, out hit)) {
+			transform.LookAt(new Vector3(hit.point.x, transform.position.y, hit.point.z));
+		}
+	}
+
+	// void FixedUpdate() {
+	// 	RaycastHit hit;
+	// 	Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+	// 	if (Physics.Raycast(ray, out hit)) {
+	// 		transform.LookAt(new Vector3(hit.point.x, transform.position.y, hit.point.z));
+	// 	}
+	// }
+}
---
+++ Assets/Scripts/David-Scripts/testMovementV2.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class testMovementV2 : MonoBehaviour {
+
+	public float speed;
+
+    public Camera cam;
+
+    public NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+
+
+                
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+        transform.Translate(speed * moveX * Time.deltaTime, 0f, speed * moveZ * Time.deltaTime, Space.World);
+        if (moveX != 0 || moveZ != 0)
+        {
+            // transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+            agent.SetDestination(transform.position);
+        }
+       
+            
+        
+
+        // if (Input.GetMouseButton(0))
+        // {
+        //     RaycastHit hit;
+        //     Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+        //     if (Physics.Raycast(ray, out hit))
+        //     {       //Now the agent is moved
+        //         agent.SetDestination(hit.point);
+
+
+        //     }
+        // }
+    }
+}
