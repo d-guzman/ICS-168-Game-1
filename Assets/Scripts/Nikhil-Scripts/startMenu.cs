a1b2c70ee5290ca5ef96121ca87654004840cd4c
Nikhil Sharma <nik41507@gmail.com>
2018-04-21 12:09:43 -0700

    Basic Menu and Teleportation
    Now there is a (very) basic main menu which can start or quit the game. We haven't arraged the build orders yet so it does not really start the game.
    
    Also there are now teleportation pads. It uses the NavMesh system to do teleportation, but works with the WASD controls. Step on the first pad and it takes you to the second. Get off second pad and step back on to arrive at the first again.
    
    There is a new scene, a new prefab, a new material, anda few new scripts
    
    -Changed tag of Player to "Player". This is necessary for the Portals to work

---
+++ Assets/Scripts/Nikhil-Scripts/startMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class startMenu : MonoBehaviour {
+
+
+    public void startGame() //Goes to the next scene. The Main Menu should have an index of one for this to load into the first level 
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void quitGame() //Quits the application
+    {
+        Debug.Log("You've quit the game");
+        Application.Quit();
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/Nikhil-Scripts/testMovement.cs
+++ Assets/Scripts/Nikhil-Scripts/testMovement.cs
@@ -13,10 +13,35 @@ public class testMovement : MonoBehaviour {
 
     public NavMeshAgent agent;
 
+    public bool movementAllowed;
+
 	// Use this for initialization
 	void Start () {
-		
+        movementAllowed = true;
 	}
+
+    public void disableMovement()
+    {
+        Debug.Log("Movement disabled");
+        movementAllowed = false;
+    }
+        //Other scripts can call this to prevent the player from moving while events, such as teleportation, are going on
+    public void enableMovement()
+    {
+        Debug.Log("Movement enabled");
+        movementAllowed = true;
+    }
+
+    public void teleportTo(Vector3 location)
+    {
+        Debug.Log("The player is at " + transform.position);
+        Debug.Log("The player should move to " + location);
+        //Vector3.Lerp(transform.position, location, 1f);
+        //transform.position = location;
+        agent.Warp(location);
+        Debug.Log("NOW player is at " + transform.position);
+
+    }
 
     // Update is called once per frame
     void Update() {
@@ -25,16 +50,19 @@ public class testMovement : MonoBehaviour {
                 
         float moveX = Input.GetAxis("Horizontal");
         float moveZ = Input.GetAxis("Vertical");
-        transform.Translate(speed * moveX * Time.deltaTime, 0f, speed * moveZ * Time.deltaTime);
-        if (moveX != 0 || moveZ != 0)
+        if(movementAllowed)
         {
-            transform.rotation = Quaternion.Euler(0f, 0f, 0f);
-            agent.SetDestination(transform.position);
+            transform.Translate(speed * moveX * Time.deltaTime, 0f, speed * moveZ * Time.deltaTime);
+            if (moveX != 0 || moveZ != 0)
+            {
+                transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+                agent.SetDestination(transform.position);
+            }
         }
+
        
             
-        
-
+     
         if (Input.GetMouseButton(0))
         {
             RaycastHit hit;
